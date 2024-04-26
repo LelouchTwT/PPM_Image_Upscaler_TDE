bc948925ea7addda270ae330204e09ab7e0a4bb7
João Pedro Antoniazi Tonello <joao.antoniazi@webcontinental.com.br>
2024-04-26 14:54:12 -0300

    refactor: melhorei estrutura do codigo

--- zoom.c
+++ zoom.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <malloc.h>
 #include "lib_ppm.h"
+
+unsigned char adjust_pixel_value(unsigned char pixel_value, int k) {
+    if (pixel_value >= 180) {
+        return 255;
+    } else if (pixel_value >= 135 && pixel_value < 180) {
+        return (k == 1) ? 0 : 255;
+    } else if (pixel_value >= 75 && pixel_value < 135) {
+        return (k == 1) ? 255 : 0;
+    }
+    return 0;
+}
+
 int main() {
     struct image_s data;
     struct image_s *image = &data;
-    int r = read_ppm("C:\\Users\\joao.antoniazi\\CLionProjects\\PPM_Image_Upscaler_TDE\\lena.ppm", image);
+    int r = read_ppm("./lena.ppm", image);
 
-    //Return if error reading PPM file
     if (r != 0) {
         printf("Error reading PPM file\n");
         return -1;
@@ -26,136 +37,29 @@ int main() {
                     new_pixel.r = 0;
                     new_pixel.g = 0;
                     new_pixel.b = 0;
-                    switch (k) {
-                        case 0:
-                            switch (l) {
-                                case 0: //red
-                                if (current_pixel.r >= 180){
-                                    new_pixel.r = 255;
-                                }else if(current_pixel.r >= 135 && current_pixel.r <= 179){
-                                    new_pixel.r = 255;
-                                }else if(current_pixel.r >= 75 && current_pixel.r <= 134){
-                                    new_pixel.r = 0;
-                                }else{
-                                    new_pixel.r = 0;
-                                }
-                                    break;
-                                case 1: //green
-                                    if (current_pixel.g >= 180){
-                                    new_pixel.g = 255;
-                                }else if(current_pixel.g >= 135 && current_pixel.g <= 179){
-                                    new_pixel.g = 255;
-                                }else if(current_pixel.g >= 75 && current_pixel.g <= 134){
-                                    new_pixel.g = 0;
-                                }else{
-                                    new_pixel.g = 0;
-                                }
-                                    break;
-                                case 2: //blue
-                                    if (current_pixel.b >= 180){
-                                        new_pixel.b = 255;
-                                    }else if(current_pixel.b >= 135 && current_pixel.b <= 179){
-                                        new_pixel.b = 255;
-                                    }else if(current_pixel.b >= 75 && current_pixel.b <= 134){
-                                        new_pixel.b = 0;
-                                    }else{
-                                        new_pixel.b = 0;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
+
+                    switch (l) {
+                        case 0: //RED
+                            new_pixel.r = adjust_pixel_value(current_pixel.r, k);
                             break;
-                        case 1:
-                            switch (l) {
-                                case 0: //red
-                                    if (current_pixel.r >= 180){
-                                        new_pixel.r = 255;
-                                    }else if(current_pixel.r >= 135 && current_pixel.r <= 179){
-                                        new_pixel.r = 0;
-                                    }else if(current_pixel.r >= 75 && current_pixel.r <= 134){
-                                        new_pixel.r = 255;
-                                    }else{
-                                        new_pixel.r = 0;
-                                    }
-                                    break;
-                                case 1: //green
-                                    if (current_pixel.g >= 180){
-                                        new_pixel.g = 255;
-                                    }else if(current_pixel.g >= 135 && current_pixel.g <= 179){
-                                        new_pixel.g = 0;
-                                    }else if(current_pixel.g >= 75 && current_pixel.g <= 134){
-                                        new_pixel.g = 255;
-                                    }else{
-                                        new_pixel.g = 0;
-                                    }
-                                    break;
-                                case 2: //blue
-                                    if (current_pixel.b >= 180){
-                                        new_pixel.b = 255;
-                                    }else if(current_pixel.b >= 135 && current_pixel.b <= 179){
-                                        new_pixel.b = 0;
-                                    }else if(current_pixel.b >= 75 && current_pixel.b <= 134){
-                                        new_pixel.b = 255;
-                                    }else{
-                                        new_pixel.b = 0;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
+                        case 1: //GREEN
+                            new_pixel.g = adjust_pixel_value(current_pixel.g, k);
                             break;
-                        case 2:
-                            switch (l) {
-                                case 0: //
-                                    if (current_pixel.r >= 180){
-                                        new_pixel.r = 255;
-                                    }else if(current_pixel.r >= 135 && current_pixel.r <= 179){
-                                        new_pixel.r = 255;
-                                    }else if(current_pixel.r >= 75 && current_pixel.r <= 134){
-                                        new_pixel.r = 0;
-                                    }else{
-                                        new_pixel.r = 0;
-                                    }
-                                    break;
-                                case 1: //green
-                                    if (current_pixel.g >= 180){
-                                    new_pixel.g = 255;
-                                }else if(current_pixel.g >= 135 && current_pixel.g <= 179){
-                                    new_pixel.g = 255;
-                                }else if(current_pixel.g >= 75 && current_pixel.g <= 134){
-                                    new_pixel.g = 0;
-                                }else{
-                                    new_pixel.g = 0;
-                                }
-                                    break;
-                                case 2: //blue
-                                    if (current_pixel.b >= 180){
-                                        new_pixel.b = 255;
-                                    }else if(current_pixel.b >= 135 && current_pixel.b <= 179){
-                                        new_pixel.b = 255;
-                                    }else if(current_pixel.b >= 75 && current_pixel.b <= 134){
-                                        new_pixel.b = 0;
-                                    }else{
-                                        new_pixel.b = 0;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
+                        case 2: //BLUE
+                            new_pixel.b = adjust_pixel_value(current_pixel.b, k);
                             break;
                         default:
                             break;
                     }
-                    new_image.pix[(i*3+k) * new_image.width + (j*3+l)] = new_pixel;
+                    new_image.pix[(i * 3 + k) * new_image.width + (j * 3 + l)] = new_pixel;
                 }
             }
         }
     }
 
-    r = write_ppm("C:\\Users\\joao.antoniazi\\CLionProjects\\PPM_Image_Upscaler_TDE\\lena_new.ppm", &new_image);
+    r = write_ppm("./lena_new.ppm", &new_image);
     free_ppm(&new_image);
-if (r != 0) {
+    if (r != 0) {
         printf("Error writing PPM file\n");
         return -1;
     }
